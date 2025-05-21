dd5d1befc991263a22710b15276257cd30e6fd35
kiwiidb <kwinten.debacker@hotmail.com>
2025-05-22 01:43:08 +0200

    add cors

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -32,11 +32,28 @@ func main() {
 	mux := http.NewServeMux()
 
 	service.RegisterRoutes(mux)
+	// add cors middleware on all routes
+	handler := corsMw(mux)
 
 	//serve static files
 	mux.Handle("/", http.FileServer(http.Dir("static")))
 
 	// Start the server
 	log.Println("Starting server on :8080")
-	log.Fatal(http.ListenAndServe(":8080", mux))
+	log.Fatal(http.ListenAndServe(":8080", handler))
+}
+
+func corsMw(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
